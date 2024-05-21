33e237b5b5946dc62cebb61e66db0f5a21dd63ea
roland-adams2007 <adamsrolly7@gmail.com>
2024-05-21 14:51:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Nav from "./restaurant/Nav";
+import Main from "./restaurant/Main";
+import Footer from "./restaurant/Footer";
+const App = ()=>{
+  return (
+    <>
+     
+     <Nav/>
+     <Main/>
+     <Footer/>
+
+    </>
+  )
+}
+
+export default App;
---
+++ src/restaurant/Main.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+ const Main = () =>{
+
+  const[categories,setCategories]=useState(null)
+
+  //create a function that will handle your request
+  const fetchFoodCategories=()=>{
+    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+     setCategories([data.categories])
+
+    })
+  }
+
+  //call the function inside use effect
+  useEffect(()=>{
+      fetchFoodCategories();
+  }, [])
+
+
+   return(
+    
+<main>
+  {/* banner section */}
+  <section
+    className="container-fluid p-0 m-0"
+    style={{
+      backgroundImage: 'url("assets/images/hero-bg.jpg")',
+      height: 300,
+      backgroundSize: "cover"
+    }}
+  >
+    <div
+      className="row"
+      style={{ backgroundColor: "rgba(0,0,0,0.7)", height: "inherit" }}
+    >
+      <div className="col">
+        <h1 className="text-center text-light mt-4" style={{ lineHeight: 5 }}>
+          Eat Healthy, Stay Healthy.
+        </h1>
+      </div>
+    </div>
+  </section>
+  {/* end banner section */}
+  <div className="album py-5 bg-light">
+    <div className="container" id="shopnow">
+      <div className="row">
+        <div className="col-md-12">
+          <h3 className="text-center mb-3">Welcome Home!</h3>
+        </div>
+      </div>
+      
+      <div className="row">
+
+        <div className="col-md-12">
+          {/* Categories of menu starts */}
+          <h5 className="mb-3 text-danger">CATEGORIES</h5>
+          {/* category item*/}
+         
+         {
+         categories && categories.map(function(cat){
+
+            return(
+              <div className="row g-3" key={cat.idCategory} id="shopnow">
+              <div className="col-md-2">
+                <img src={cat.strCategoryThumb} alt="" className="img-fluid" />
+              </div>
+              <div className="col-md-8">
+                <h5> {cat.strCategory}</h5>
+                <p>
+                  {cat.strCategoryDescription}
+                </p>
+          
+              </div>
+              <hr />
+            </div>
+            )
+          })
+         }
+
+        </div>
+
+        {/* end category item*/}
+      
+      </div>
+      {/* end category item*/}
+      {/* Categories of menu ends */}
+    </div>
+  </div>
+</main>
+
+
+   )
+
+ }
+
+ export default Main
